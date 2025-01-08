04b4e45e42b0d0cbf77a3af6bfca623ae57879f8
Ivan C <chocochipahs@gmail.com>
2025-01-08 06:42:45 -0600

    Add fetching exercises from database

--- src/app/workouts/create/page.tsx
+++ src/app/workouts/create/page.tsx
@@ -1,3 +1,33 @@
-export default function CreateWorkout() {
-    return <h1>Create Workout</h1>
+import { createClient } from "@/utils/supabase/server";
+
+export default async function CreateWorkout() {
+    const supabase = await createClient();
+
+    const { data: exercises, error} = await supabase
+        .from('exercises')
+        .select('*')
+        // FIXME: Random 4
+        .limit(4)
+    
+    if (error) {
+        console.log("Error: ", error);
+        return <div>Error loading exercises.</div>
+    }
+
+    console.log(exercises);
+
+   return (
+        <div>
+            <h1>Create Workout</h1>
+            <div>
+                {exercises.map((exercise) => (
+                    <div key={exercise.id}>
+                        <p>{exercise.name}</p>
+                        <p>{exercise.description}</p>
+                        <p>{exercise.category}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+   )
 }
